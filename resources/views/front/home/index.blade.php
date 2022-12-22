e983921bebd573ee9e2b68b6d116a10b8a5959c0
Aounhassan275 <aounhassan275@gmail.com>
2022-12-22 19:04:11 +0500

    Fix

--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -4,20 +4,44 @@
 @endsection
 @section('content')
 <section class="event_part section_padding">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12">
-          <div class="event_slider owl-carousel">
-            @foreach(App\Models\Slider::orderBy('display_order')->get() as $slider)
-            <div class="single_event_slider">
-              <img src="{{asset($slider->image)}}" alt="">
-            </div>
-            @endforeach
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+	<div class="container">
+		<div class="row">
+		<div class="col-lg-12">
+			<div class="event_slider owl-carousel">
+			@foreach(App\Models\Slider::orderBy('display_order')->get() as $slider)
+			<div class="single_event_slider" style="background-image:url({{asset($slider->image)}})!important;height:500px!important;">
+				{{-- <img src="" alt=""> --}}
+				{{-- <p>Adventure</p> --}}
+				{{-- <h1 style="text-align:center;">Adventure</h1> --}}
+			</div>
+			@endforeach
+			</div>
+		</div>
+		</div>
+	</div>
+</section>
+
+<section class="best_services section_padding">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-xl-6">
+				<div class="section_tittle text-center">
+					<h2>We Offered Best Services</h2>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			@foreach(App\Models\Category::all()->take(6) as $category)
+			<div class="col-lg-4 col-sm-6">
+				<div class="single_ihotel_list">
+					<img src="{{asset($category->image)}}" alt="">
+					<h3> <a href="{{route('category.show',str_replace(' ', '_',$category->name))}}"> {{$category->name}}</a></h3>
+				</div>
+			</div>
+			@endforeach
+		</div>
+	</div>
+</section>
 <section class="top_place section_padding">
 	<div class="container">
 		<div class="row justify-content-center">
@@ -28,8 +52,11 @@
 				</div>
 			</div>
 		</div>
+		@php 
+		$desert_safari_category = App\Models\Category::where('name','Desert Safari')->first();
+		@endphp
 		<div class="row">
-			@foreach(App\Models\Service::orderBy('display_order')->get()->take(8) as $service)
+			@foreach(App\Models\Service::where('category_id',@$desert_safari_category->id)->orderBy('display_order')->get()->take(8) as $service)
 			<div class="col-lg-6 col-md-6">
 				<div class="single_place">
 					<img src="{{asset(@$service->images->first()->image)}}" alt="">
@@ -129,27 +156,6 @@
             </div>
         </div>
     </div>
-</section>
-<section class="best_services section_padding">
-	<div class="container">
-		<div class="row justify-content-center">
-			<div class="col-xl-6">
-				<div class="section_tittle text-center">
-					<h2>We Offered Best Services</h2>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			@foreach(App\Models\Category::all()->take(6) as $category)
-			<div class="col-lg-4 col-sm-6">
-				<div class="single_ihotel_list">
-					<img src="{{asset($category->image)}}" alt="">
-					<h3> <a href="{{route('category.show',str_replace(' ', '_',$category->name))}}"> {{$category->name}}</a></h3>
-				</div>
-			</div>
-			@endforeach
-		</div>
-	</div>
 </section>
 <section class="client_review section_padding">
 	<div class="container">
